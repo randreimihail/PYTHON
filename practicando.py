5c7239d6940ae917269e5887d2a4d9f39fa829c5
randreimihail <randreimihail@outlook.com>
2024-11-11 13:31:20 +0100

    v1.6

---
+++ practicando.py
@@ -0,0 +1,6 @@
+lista_claves=["nombre","edad"]
+lista_valores=["Angel", 43]
+diccionario=dict(zip(lista_claves, lista_valores))
+diccionario["ciudad"]="Caspe"
+print(diccionario)
+print(len(diccionario))
